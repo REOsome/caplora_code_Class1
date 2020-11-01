514574ddd5f062020e60b66761079370fc3039b1
unknown <OdhiamboREO@gmail.com>
2020-11-01 18:37:51 +0300

    Assgn

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ class calculator {
     addNumbers(number) {
         if (this.sym !== null) {
 
-            this.number2 = parseInt(this.number2.toString() + number)
+            this.number2 = parseInt(this.number2.toString() + number)        
+            document.getElementById('number2').innerHTML = this.number2
         } else {
             this.number1 = parseInt(this.number1.toString() + number)
+            document.getElementById('number1').innerHTML = this.number1
         }
+        document.getElementById('symbol').innerHTML = this.sym
+        
+        // this.sym !== null ? this.number2 = parseInt(this.number2.toString() + number) :( this.number1 = parseInt(this.number1.toString() + number))
+
+        
     }
     execute() {
+
+        console.log(123);
         switch (this.sym) {
             case 'ádd':
                 return this.add()
@@ -22,20 +31,47 @@ class calculator {
             case 'mod':
                 return this.mod()
                 break;
-
+            case 'subtract':
+                return this.subtract()
+                break;
+            case 'division':
+                return this.division()
+                break;
+            case 'multiplication':
+                return this.multiplication()
+                break; 
             default:
                 return 'érror'
                 break;
         }
     }
     addsymbol(sym) {
-        this.sym = sym
+        sym == 'clear' ? this.clear()  :  this.sym = sym
     }
     add() {
         return this.number1 + this.number2
     }
     mod() {
         return this.number1 % this.number2;
+    }
+    subtract() {
+        return this.number1 - this.number2;
+    }
+    division() {
+        return this.number1 / this.number2;
+    }
+    multiplication() {
+        return this.number1 * this.number2;
+    }
+    clear() {
+        console.log(12);
+        this.number1 = 0
+        this.number2 = 0
+        this.sym = null
+        
+        document.getElementById('number1').innerHTML = this.number1
+        
+        document.getElementById('number2').innerHTML = this.number2
     }
 }
 let calc = new calculator();
@@ -50,5 +86,8 @@ function handlesym(params) {
 }
 let execute = () => {
     console.log(calc.execute())
+}
+let result = () => {
+    return execute
 }
 
