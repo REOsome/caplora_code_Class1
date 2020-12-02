8c5b4ea12a0f691bb69e3760023103bef58f7cbb
unknown <OdhiamboREO@gmail.com>
2020-12-02 17:33:45 +0300

    firebase

--- todo/src/index.js
+++ todo/src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from "firebase/app";
 
+
+ export const config =  {
+  apiKey: "AIzaSyAEjbn-38w-atN0k2jBHfGcFkq73exZa4U",
+  authDomain: "to-do-5622d.firebaseapp.com",
+  databaseURL: "https://to-do-5622d.firebaseio.com",
+  projectId: "to-do-5622d",
+  storageBucket: "to-do-5622d.appspot.com",
+  messagingSenderId: "1078066602031",
+  appId: "1:1078066602031:web:6a99d24c8141d6683fa6da",
+  measurementId: "G-X8Z4CS5GVW"
+};
+
+firebase.initializeApp(config);
 ReactDOM.render(
   <React.StrictMode>
     <App />
